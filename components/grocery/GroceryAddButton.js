ef60d02cdde3ac4c6d35aef5febcfc42e43630f8
Joshua Couto <joshua.s.couto@gmail.com>
2026-04-10 13:34:41 -0600

    feat: add GroceryAddButton component

--- components/grocery/GroceryAddButton.js
+++ components/grocery/GroceryAddButton.js
@@ -1,3 +1,5 @@
+"use client";
+
 /**
  * @fileoverview A component which persists over top every element on the page.
  * The user can click on this button to add a grocery item to the list.
@@ -5,8 +7,12 @@
  * @version 1.0.0
  */
 
-export default function GroceryAddButton() {
+export default function GroceryAddButton({ onOpenModal }) {
   return (
-
+    <button className="fixed bottom-6 right-6 w-14 h-14 rounded-full 
+        bg-blue-500 text-white text-2xl shadow-lg hover:bg-blue-600"
+      onClick={onOpenModal}>
+      +
+    </button>
   );
 }
---
+++ components/grocery/GroceryAddModal.js
@@ -0,0 +1,13 @@
+"use client";
+
+/**
+ * @fileoverview A modal which appears after the user has clicked on the 
+ * GroceryAddButton. This modal will allow the user to add a new item to
+ * the grocery list by entering the item name, quantity, and category
+ * @author Joshua Couto
+ * @version 1.0.0
+ */
+
+export default function GroceryAddModal({ isOpen, onConfirm, onCancel }) {
+  return null;
+}
